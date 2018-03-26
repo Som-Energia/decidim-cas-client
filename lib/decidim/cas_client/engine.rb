62dfadcad13bce73231ff0bc740f5c66c0791f8d
Marc Reniu <marc.rs@coditramuntana.com>
2018-03-26 15:52:43 +0200

    [FIX] Added precompile options. Modified User email index

--- lib/decidim/cas_client/engine.rb
+++ lib/decidim/cas_client/engine.rb
@@ -18,6 +18,13 @@ module Decidim
         root to: "authorizations#new"
       end
 
+      initializer "decidim.assets" do |app|
+        app.config.assets.precompile += %w(decidim_cas_client_manifest.js)
+        app.config.assets.precompile += %w( decidim/cas_client/somenergia-icon.png )
+
+        app.config.assets.debug = true if Rails.env.test?
+      end
+
       initializer :append_migrations do |app|
         unless app.root.to_s.match root.to_s
           config.paths["db/migrate"].expanded.each do |expanded_path|
