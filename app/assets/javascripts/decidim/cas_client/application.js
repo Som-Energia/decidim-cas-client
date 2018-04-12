82dfd81304e259446d6115c243e815bb45c41c16
Marc Reniu <marc.rs@coditramuntana.com>
2018-04-12 11:39:27 +0200

    [FIX] Search&Modify login/logout routes by HREF attr

--- app/assets/javascripts/decidim/cas_client/application.js
+++ app/assets/javascripts/decidim/cas_client/application.js
@@ -1,5 +1,6 @@
 $(function(){
   var locale = $(document.documentElement).attr("lang") || "es";
-  $(".sign-in-link").attr('href', '/users/cas/sign_in?locale='+locale);
-  $(".sign-out-link").attr('href', '/users/cas/sign_out?locale='+locale);
+  $("a[href*='/users/sign_in']").attr('href', '/users/cas/sign_in?locale='+locale);
+  $("a[href*='/users/sign_up']").attr('href', '/users/cas/sign_in?locale='+locale);
+  $("a[href*='/users/sign_out']").attr('href', '/users/cas/sign_out?locale='+locale);
 });
