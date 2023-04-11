9725d6be091e1eb3dab6d65cf5d24887cdb77931
Ivan Vergés <ivan@pokecode.net>
2023-04-11 14:29:35 +0200

    add system tests

--- app/assets/javascripts/decidim/cas_client/hide_show_login.js
+++
@@ -1,5 +0,0 @@
-const changeVisibilityLoginBox = () => {
-  var element = $('.login__box');
-  var display = element.css('display') == 'block' ? 'none' : 'block';
-  element.css('display', display);
-};
