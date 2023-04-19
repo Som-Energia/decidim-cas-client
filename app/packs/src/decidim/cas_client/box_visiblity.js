a88d6e36ee1aa3962574c41db70164e50f1185b7
Ivan Vergés <ivan@pokecode.net>
2023-04-19 18:18:59 +0200

    avoid jquery

--- app/packs/src/decidim/cas_client/box_visiblity.js
+++ app/packs/src/decidim/cas_client/box_visiblity.js
@@ -1,5 +1,5 @@
 window.changeVisibilityLoginBox = () => {
-  var element = $('.login__box');
-  var display = element.css('display') == 'block' ? 'none' : 'block';
-  element.css('display', display);
+  var element = document.querySelector('.login__box');
+  var display = element.style.display == 'block' ? 'none' : 'block';
+  element.style.display = display;
 };
